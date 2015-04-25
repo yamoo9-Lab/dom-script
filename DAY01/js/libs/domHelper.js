b308a18e302b8cab890ad7c921f359692c7d8d40
Jee Hoon <yamoo9@naver.com>
2015-04-25 12:39:41 +0900

    라이브러리 원리~ 설명

---
+++ DAY01/js/libs/domHelper.js
@@ -0,0 +1,8 @@
+// 맛보기!! 크로스 브라우징 헬퍼 함수를 만들어보자!!!
+function getStyle(el, prop) {
+	if (window.getComputedStyle) {
+		return window.getComputedStyle(el)[prop];
+	} else {
+		return el.currentStyle[prop]
+	}
+}
