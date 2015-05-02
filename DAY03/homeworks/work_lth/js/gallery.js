e5728376e1e4e7d6791cb7faf4f922a56ef30f90
Jee Hoon <yamoo9@naver.com>
2015-05-02 12:20:32 +0900

    DOM 탐색 메소드 코드 추가 01

--- DAY03/homeworks/work_lth/js/gallery.js
+++ DAY03/homeworks/work_lth/js/gallery.js
@@ -1,57 +1,57 @@
 var body = document.body;
 // var bodyImg = body.getAttribute('background');
 
-var thumbnails = document.querySelectorAll('.gallery-item');
+var thumbnails = document.querySelectorAll('.gallery-item'); // img
 
 // var showBox = document.querySelector('.show-box');
 // var showBoxPath = showBox.getAttribute('src');
 
-// thumbnails[0].onclick = function() {	
+// thumbnails[0].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[1].onclick = function() {	
+// thumbnails[1].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[2].onclick = function() {	
+// thumbnails[2].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[3].onclick = function() {	
+// thumbnails[3].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[4].onclick = function() {	
+// thumbnails[4].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[5].onclick = function() {	
+// thumbnails[5].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[6].onclick = function() {	
+// thumbnails[6].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[7].onclick = function() {	
+// thumbnails[7].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[8].onclick = function() {	
+// thumbnails[8].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
 // }
-// thumbnails[9].onclick = function() {	
+// thumbnails[9].onclick = function() {
 // 	// console.log(this.src)
 // 	body.style.background = 'url('+this.src+') top left /cover no-repeat'
 // 	return false;
@@ -59,12 +59,17 @@ var thumbnails = document.querySelectorAll('.gallery-item');
 
 var thumbnailsCount = thumbnails.length;
 
+// console.log(thumbnailsCount);
+
 while (thumbnailsCount > 0) {
-	thumbnailsCount--
-	thumbnails[thumbnailsCount].onclick = changeBG
+	thumbnailsCount--;
+	// console.log(thumbnails[thumbnailsCount].parentNode);
+	thumbnails[thumbnailsCount].parentNode.onclick = changeBG;
+	// console.log(thumbnailsCount);
 }
 
 function changeBG() {
-	body.style.background = 'url('+this.src+') top left /cover no-repeat'
+	var src = this.querySelector('img').getAttribute('src');
+	body.style.background = 'url(' + src + ') top left /cover no-repeat';
 	return false;
 }
--- DAY03/homeworks/work_lth/js/gallery_showbox.js
+++ DAY03/homeworks/work_lth/js/gallery_showbox.js
@@ -1,8 +1,8 @@
-var imgChip = document.querySelectorAll('.gallery-item');
+var imgChip     = document.querySelectorAll('.gallery-item');
 var imgChipList = imgChip.length;
 
 var showBoxImg = document.getElementById('show-box');
-var showBox = document.querySelector('section');
+var showBox    = document.querySelector('section');
 
 while (imgChipList > 0) {
 	imgChipList--
@@ -12,10 +12,11 @@ function gallery() {
 	// showBoxImg.src = this.src;
 	// showBoxImg.style.height = window.innerHeight + 'px';
 	// showBoxImg.style.width = window.innerWidth + 'px';
-	showBox.style.background = 'url(' + this.src +') center center no-repeat';
+	showBox.style.backgroundImage = 'url(' + this.src +')';
 	showBox.style.height = window.innerHeight + 'px';
-	showBox.style.width = window.innerWidth + 'px';
-	
+	// showBox.style.width = window.innerWidth + 'px';
+	// showBox.style.backgroundSize = 'cover';
+
 	return false;
 }
 
