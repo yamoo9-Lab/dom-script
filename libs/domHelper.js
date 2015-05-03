f544ac6886a434b52e42963ac61d743ac144ea42
Jee Hoon <yamoo9@naver.com>
2015-05-03 14:34:07 +0900

    DOM 헬퍼 코드 추가

--- libs/domHelper.js
+++ libs/domHelper.js
@@ -6,3 +6,34 @@ function getStyle(el, prop) {
 		return el.currentStyle[prop];
 	}
 }
+
+function addTitle(el, title) {
+		if ( el.nodeType === 1 ) {
+			el.setAttribute('title', title);
+		}
+	}
+
+function hasClass(el, className) {
+	if (el.nodeName) {
+		var classList = el.className.split(' '),
+			l         = classList.length;
+		while(--l > -1) {
+			if (classList[l] === className) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+function addClass(el, className) {
+	if ( el.nodeName && !hasClass(el, className) ) {
+		el.className += ' ' + className;
+	}
+}
+
+function removeClass(el, className) {
+	if (el.nodeName && hasClass(el, className) ) {
+		el.className = el.className.replace(className, '').trim();
+	}
+}
