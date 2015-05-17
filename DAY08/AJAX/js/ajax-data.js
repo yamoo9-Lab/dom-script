390d4066d2e7a2917f4e887610a598b6caaba36e
Jee Hoon <yamoo9@naver.com>
2015-05-17 11:04:56 +0900

    AJAX 코드 추가

---
+++ DAY08/AJAX/js/ajax-data.js
@@ -0,0 +1,12 @@
+/*! ajax-data.js © yamoo9.net, 2015 */
+
+;(function(global){
+	'use strict';
+
+	// AJAX를 처리해주는 객체를 활용
+	// XMLHttpRequest 객체 생성 -> 인스턴스
+	var xhr = new XMLHttpRequest;
+
+	console.log(xhr);
+
+})(window);
