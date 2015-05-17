0546d3b646f71d4ed99d06a7541ac176ed494877
Jee Hoon <yamoo9@naver.com>
2015-05-17 11:32:51 +0900

    AJAX 흐름 코드 추가

--- DAY08/AJAX/js/ajax-data.js
+++ DAY08/AJAX/js/ajax-data.js
@@ -6,7 +6,29 @@
 	// AJAX를 처리해주는 객체를 활용
 	// XMLHttpRequest 객체 생성 -> 인스턴스
 	var xhr = new XMLHttpRequest;
+	var callDataZone = document.querySelector('#called-data-zone');
+	var callAjaxBtn = document.querySelector('#call-ajax-data');
 
-	console.log(xhr);
+	// xhr 인스턴스(조각) : AJAX 통신 객체
+	// .open(방법, 파일, 비동기)
+	xhr.open('GET', '/AJAX/data/data.txt');
+
+	// 통신합니다.
+	callAjaxBtn.onclick = function() {
+		xhr.send();
+		return false;
+	};
+
+	// 통신한 결과 (이벤트 발생, 감지, 처리)
+	xhr.onreadystatechange = function() {
+		if(this.status === 200 && this.readyState === 4) {
+			var data = this.responseText;
+			callDataZone.innerHTML = data;
+		}
+	};
+
+	// xhr.addEventListener('readystatechange', function() {
+		// 진보된 이벤트 모델
+	// });
 
 })(window);
