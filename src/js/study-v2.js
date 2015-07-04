9df05c1ed0dc5dd38c832d5ac916ac589371a759
Jee Hoon <yamoo9@naver.com>
2015-07-04 14:55:44 +0900

    크로스 브라우징을 위한 코드 분기

--- src/js/study-v2.js
+++ src/js/study-v2.js
@@ -26,15 +26,32 @@ window.onload = function() {
 		// this.innerText란?
 		// this는 buttons[0] 요소를 가리키고,
 		// this.innerText는 buttons[0]이 포함한 텍스트를 말합니다.
-		body.style.backgroundColor = this.innerText;
+		// body.style.backgroundColor = this.innerText;
+
+		// W3C 표준 방식
+		// var bgColor = window.getComputedStyle(this).backgroundColor;
+		// MS IE 비표준 방식
+		// var bgColor = this.currentStyle.backgroundColor;
+		var bgColor = y9.getStyle(this, 'backgroundColor');
+		body.style.backgroundColor = bgColor;
 	};
 	// 두번째 버튼
 	buttons[1].onclick = function() {
-		body.style.backgroundColor = this.innerText;
+		// body.style.backgroundColor = this.innerText;
+
+		// W3C 표준 방식
+		// var bgColor = window.getComputedStyle(this).backgroundColor;
+		var bgColor = y9.getStyle(this, 'backgroundColor');
+		body.style.backgroundColor = bgColor;
 	};
 	// 세번째 버튼
 	buttons[2].onclick = function() {
-		body.style.backgroundColor = this.innerText;
+		// body.style.backgroundColor = this.innerText;
+
+		// W3C 표준 방식
+		// var bgColor = window.getComputedStyle(this).backgroundColor;
+		var bgColor = y9.getStyle(this, 'backgroundColor');
+		body.style.backgroundColor = bgColor;
 	};
 
 };
