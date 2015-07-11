a8e66ea71604b6e9a047e7fc3cf4961a2fcb90b7
Jee Hoon <yamoo9@naver.com>
2015-07-11 15:04:12 +0900

    split(), join() 구문 사용

--- src/js/index.js
+++ src/js/index.js
@@ -10,22 +10,27 @@ window.onload = init;
 // console.log('callback temp function');
 
 // 함수 이름을 이벤트에 연결할 때는 왜? 실행 연산자 ()를 붙이지 않는가?
-document.onclick = temp;
+// document.onclick = temp;
 
-function temp() {
-	console.log('callback temp function');
-}
+// function temp() {
+// 	console.log('callback temp function');
+// }
 
 // 초기화 함수
 function init() {
 	// 문서에서 #controls를 찾아라.
-	var controls = document.getElementById('controls');
+	var controls = document.querySelector('#controls');
+	var wallpages = document.querySelector('#wallpages');
+
+	// console.log(wallpages);
 
 	// console 패널에 찾은 #controls를 기록(log)해봐라.
 	// console.log(controls);
 
 	// controls(변수가 참조하는 문서 객체) 내부에 존재하는 버튼(<a>)을 찾아라.
-	var controls_btns = controls.getElementsByTagName('a');
+	var controls_btns = controls.querySelectorAll('a');
+	var wallpages_divs = wallpages.querySelectorAll('div');
+	console.log(wallpages_divs);
 	// ['a', 'a', 'a', 'a']
 	// console.log(controls_btns[0]); // #controls 내부에 있는 <a> 요소의 집합
 
@@ -41,11 +46,24 @@ function init() {
 	function btnAction() {
 		// this 참조는 function 내부에서 사용될 때, 이벤트에 연결된 문서 객체를 가리킨다.
 		// class 속성은 className으로 써야 한다.
-		console.log(this.getAttribute('class'));
-		console.log(this.getAttribute('id'));
+		// console.log(this.getAttribute('class'));
+		// console.log(this.getAttribute('id'));
 		// console.log(this['aria-label']);
 		// aria-label 속성을 출력해본다.
-		console.log(this.getAttribute('aria-label'));
+		// console.log(this.getAttribute('aria-label'));
+
+		// 버튼의 속성 data-image-path 값을 가져와 변수 image_path에 참조합니다.
+		var image_path = this.getAttribute('data-image-path');
+		var my_class = this.getAttribute('class');
+		my_class = '.' + my_class.split(' ').join('.');
+		// console.log(my_class);
+		var target = wallpages.querySelector( my_class );
+		// console.log(target);
+		// 버튼의 인라인 스타일 background 속성 값으로 url() 구문 내부에 변수 image_path 값을 접합해서 적용합니다.
+		target.style.background = "url(" + image_path + ")";
+		// 버튼의 인라인 스타일 background 속성 값으로 cover를 설정합니다.
+		target.style.backgroundSize = 'cover';
+		// 브라우저 기본 동작 차단
 		return false;
 	};
 
