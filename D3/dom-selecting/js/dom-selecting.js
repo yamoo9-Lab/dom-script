ef31e9a8eafc7bf3ff79da4ae8b49b3a8208179b
Jee Hoon <yamoo9@naver.com>
2015-07-18 12:55:18 +0900

    switch ~ case 문 코드 추가

---
+++ D3/dom-selecting/js/dom-selecting.js
@@ -0,0 +1,50 @@
+// 문서에서 대상을 선택하는 방법
+// 옛날 DOM 방식
+var body = document.body;
+// Element Name (tagName)으로 찾는 방법
+// var body = document.getElementsByTagName('body').item(0);
+var body = document.getElementsByTagName('body')[0]; // 노드리스트(집합)
+// id 속성 값으로 찾는 방법
+var app = document.getElementById('app');
+// class 속성 값으로 찾는 방법 IE 9+
+var memo1 = document.getElementsByClassName('memo-1')[0]; // 노드리스트(집합)
+
+// IE 8 이상 브라우저에서 전혀 문제가 없는 방법
+// jQuery 힌트(Sizzle) -> CSS 선택자를 사용해서 대상을 선택하는 방법
+// CSS 선택자를 질의하다(질문한다)
+// .querySelector('CSS 선택자 표현식')    // 단수
+// .querySelectorAll('CSS 선택자 표현식') // 복수
+
+var body = document.querySelector('body');
+var app  = document.querySelector('#app');
+var memo = document.querySelectorAll('[class*="memo-"]');
+
+// 쉽게 사용할 수 있는 DOM 헬퍼함수를 만들어 사용한다.
+// 함수 정의 , 표현
+function selector(css_selector_ex) {
+	var list = document.querySelectorAll(css_selector_ex),
+		list_len = list.length;
+
+	switch(list_len) {
+		case 0:
+			return null;
+		break;
+		case 1:
+			return list[0];
+		break;
+	}
+
+	return list;
+
+	// if ( list_len === 1 ) {
+	// 	return list[0];
+	// } else if ( list_len === 0 ) {
+	// 	return null;
+	// } else {
+	// 	return list;
+	// }
+
+}
+
+var app = selector('#app')[0];
+var memo = selector('[class*=memo-]');
