6882b66946d76d2383883df301a0eb28be3ffa26
Jee Hoon <yamoo9@naver.com>
2015-07-25 14:01:03 +0900

    레알 돔스크립트는 쌩 노가다.

--- D4/js/index.js
+++ D4/js/index.js
@@ -1,4 +1,4 @@
-/**
+/*
  * 딤 생성 버튼 (동적으로 추가)
  * --------------------------------
  */
@@ -10,7 +10,7 @@ var dimBtnTxt = createText('딤처리');
 dimBtn.setAttribute('class', 'create-dim');
 
 // 헬퍼 함수 attr() 활용해서 dimBtn에 속성 설정
-attr(dimBtn, 'id', 'dimBtn');
+// attr(dimBtn, 'id', 'dimBtn');
 attr(dimBtn, 'class', 'dim-button');
 
 // console.log(attr(dimBtn, 'id'));
@@ -30,6 +30,59 @@ dimBtn.onclick = function() {
 };
 
 /**
- * 딤 생성
+ * 레이어 팝업 생성
  * --------------------------------
  */
+
+// .popup-layer
+var popLayer = createEl('div');
+
+attr(popLayer, 'class', 'popup-layer');
+
+append($('body'), popLayer);
+
+// .popup-layer-title
+var popLayerTitle = createEl('h3');
+var popLayerTitleTxt = createText('Do you want Save this?');
+
+append(popLayerTitle, popLayerTitleTxt);
+append(popLayer, popLayerTitle);
+
+// .popup-layer-desc
+var popLayerDesc = createEl('p');
+var popLayerDescTxt = createText('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos.');
+
+append(popLayerDesc, popLayerDescTxt);
+append(popLayer, popLayerDesc);
+
+// .popup-layer-btnset
+var popLayerbtnset = createEl('div');
+attr(popLayerbtnset, 'class', 'popup-layer-btnset');
+append(popLayer, popLayerbtnset);
+
+// .popup-layer-btnset-save
+var popLayerbtnsetSave = createEl('button');
+var popLayerbtnsetSaveTxt = createText('Save');
+attr(popLayerbtnsetSave, 'class', 'popup-layer-btnset-save');
+attr(popLayerbtnsetSave, 'type', 'button');
+
+append(popLayerbtnsetSave, popLayerbtnsetSaveTxt);
+
+var popLayerbtnsetCancel = createEl('button');
+var popLayerbtnsetCancelTxt = createText('Cancel');
+attr(popLayerbtnsetCancel, 'class', 'popup-layer-btnset-cancel');
+attr(popLayerbtnsetCancel, 'type', 'button');
+
+append(popLayerbtnsetCancel, popLayerbtnsetCancelTxt);
+
+append(popLayerbtnset, popLayerbtnsetSave);
+append(popLayerbtnset, popLayerbtnsetCancel);
+
+var popLayerClose = createEl('button');
+var popLayerCloseTxt = createText('X');
+
+attr(popLayerClose, 'class', 'popup-layer-close');
+attr(popLayerClose, 'aria-label', '팝업 레이어 닫기');
+
+append(popLayerClose, popLayerCloseTxt);
+append(popLayer, popLayerClose);
