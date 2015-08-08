6a0521078b34c27eb7a6fef26befc0ffd6fe52d0
Jee Hoon <yamoo9@naver.com>
2015-08-08 12:06:41 +0900

    내비게이션 디자인 완성 코드 추가

--- D5/js/main.js
+++ D5/js/main.js
@@ -1,9 +1,10 @@
 (function(global, _){
 
 	// DOM 객체 참조
-	var gnb     = _.$('#gnb'),
-		gnb_lv1 = _.$('.lv-1', gnb),
-		gnb_lv2 = _.$('.lv-2', gnb_lv1);
+	var gnb           = _.$('#gnb'),
+		gnb_lv1       = _.$('.lv-1', gnb),
+		gnb_lv2       = _.$('.lv-2', gnb_lv1),
+		gnb_last_link = _.last(gnb, 'a');
 
 	// 이벤트 핸들링 대상 찾기
 	var lv1_li = _.children( gnb_lv1, 'li' );
@@ -18,11 +19,14 @@
 		item_a.onfocus = openMenu;
 	});
 
+	// 마지막 a 요소 blur 이벤트 발생 시, closeMenu 함수 실행
+	gnb_last_link.onblur = closeMenu.bind( _.parent(gnb_last_link, 3) );
+
 	// 이벤트 핸들러 [메뉴 열기 | 메뉴 닫기]
 	function openMenu() {
 		var node = this.nodeName.toLowerCase(); // li, a
 		if (node === 'a') {
-			_.addClass( _.parent(this) , 'on');
+			_.radioClass( _.parent(this) , 'on');
 		} else {
 			_.addClass(this, 'on');
 		}
