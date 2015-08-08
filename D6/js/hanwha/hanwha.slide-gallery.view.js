eb236a06a9a7dfd06ef9a0193e0cb9a8e6d64601
Jee Hoon <yamoo9@naver.com>
2015-08-08 17:41:29 +0900

    View 코드 리팩토링

--- D6/js/hanwha/hanwha.slide-gallery.view.js
+++ D6/js/hanwha/hanwha.slide-gallery.view.js
@@ -9,12 +9,22 @@
 // IIFE 패턴(캡슐화)
 (function(global, hw){
 
-	// 모델 객체에 접근, hw.model (window.hanwha.model)
+	/**
+	 * --------------------------------
+	 * 모델
+	 * 모델 객체에 접근, hw.model (window.hanwha.model)
+	 */
 	var model = hw.model;
 
-	// 모델을 탐색하여 원하는 인덱스의 데이터를 출력
-	// 모델을 탐색하려면 반복문... 처리
+	/**
+	 * --------------------------------
+	 * 뷰 함수
+	 */
 
+	/**
+	 * 템플릿 제작 함수
+	 * @param  {Object} options 사용자 정의 설정 객체
+	 */
 	function makeTemplate(options) {
 		// 문서의 컨테이너 요소 참조
 		var container = document.querySelector(options.container);
@@ -33,6 +43,12 @@
 		container.innerHTML = options.template;
 	}
 
+	/**
+	 * --------------------------------
+	 * 뷰 초기 설정(Initialization)
+	 */
+
+	// makeTemplate() 함수를 사용하여 동적으로 HTML 콘텐츠 추가
 	makeTemplate({
 		'model'      : model,
 		'container'  : '#hanwha-contents',
@@ -49,71 +65,30 @@
 		'end_code'   : '</ul>'
 	});
 
-
-	/**
-	 * --------------------------------
-	 * forEach문 예시
-	 * 배열의 forEach문은 ECMAScript v5 추가
-	 * 크로스 브라우징 하려면....
-	 * es5shim.js를 사용하면 됩니다.
-	 */
-
-	// // 문서의 컨테이너 요소 참조
-	// var container = document.querySelector('#hanwha-contents');
-
-	// // HTML 템플릿 코드(문자열)
-	// var template = '';
-
-	// template += '<ul class="hw-gallery-container">';
-
-	// // View 템플릿 코드 작성
-	// model.forEach(function(item, index){
-	// 	template += '<li class="hw-gallery-item">';
-	// 	template += 	'<a href="'+ item.href +'">';
-	// 	template += 		'<img src="'+ item.image +'" alt>';
-	// 	template += 		'<h3 class="hw-gallery-headline">'+ item.headline +'</h3>';
-	// 	template += 		'<p class="hw-gallery-summary">'+ item.summary +'</p>';
-	// 	template += 	'</a>';
-	// 	template += '</li>';
-	// });
-
-	// template += '</ul>';
-
-	// // 컨테이너 요소에 template 코드 추가
-	// container.innerHTML = template;
-
-
-
-
-	/**
-	 * --------------------------------
-	 * for문 예시
-	 */
-	// for ( var i=0, l=model.length; i<l; i++ ) {
-	// 	var data = model[i];
-	// 	console.log(
-	// 		data.href,
-	// 		'\n' + data.image,
-	// 		'\n' + data.headline,
-	// 		'\n' + data.summary
-	// 	);
-	// }
-
-	var wrapper = document.querySelector('#hanwha-contents'),
-		container = wrapper.querySelector('.hw-gallery-container'),
+	// View 문서 요소 객체 참조
+	var wrapper        = document.querySelector('#hanwha-contents'),
+		container      = wrapper.querySelector('.hw-gallery-container'),
 		container_item = container.querySelector('.hw-gallery-item');
 
+	// wrapper 요소에 클래스 설정
 	wrapper.classList.add('wrapper');
 	wrapper.classList.add('hw-gallery');
 
-	var container_width = container_item.clientWidth * model.length + 'px';
+	// container 요소에 width 설정
+	var container_width = container_item.clientWidth * model.length;
+	container.style.width = container_width + 'px';
 
-	container.style.width = container_width;
 
-
-	// 뷰 객체 생성
+	/**
+	 * --------------------------------
+	 * 뷰 객체 생성
+	 */
 	hw.view = {
-		'makeTemplate': makeTemplate
+		'makeTemplate'    : makeTemplate,
+		'wrapper'         : wrapper,
+		'container'       : container,
+		'container_width' : container_width,
+		'container_item'  : container_item,
 	};
 
 })(window, (window.hanwha = window.hanwha || {}) );
